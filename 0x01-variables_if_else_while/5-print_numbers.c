566c6cbfda2855f517a3aa4b2b8829538516bc11
Terenceonye <onyeweketernce@gmail.com>
2022-07-01 12:32:35 -0400

    code updated numbers 0123...

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+	while (number < 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	println("\n");
+
+	return (0);
+}
+
+
