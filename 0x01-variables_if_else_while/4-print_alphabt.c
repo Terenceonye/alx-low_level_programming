ad9140e8d2ad2f82f9f60deac1d43489494ca738
Terenceonye <onyeweketerence@gmail.com>
2022-07-01 15:02:18 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (character = 'a'; character <= 'z'; character++)
 	{
-		if (character == 'e' || letter == 'q')
+		if (character == 'e' || character == 'q')
                         continue;
                         else
 			putchar(character);
