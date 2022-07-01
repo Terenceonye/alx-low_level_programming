cb19bed740d9ef92e3caa0a80d6329b73ba17c77
Terenceonye <onyeweketernce@gmail.com>
2022-07-01 12:45:38 -0400

    code updated numbers 0123.. using putchar.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints 0 to 9 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar("%d", number);
+		number++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
