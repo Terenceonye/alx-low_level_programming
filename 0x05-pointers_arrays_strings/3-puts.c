b576ed728d265a9ae254fcd693c416b10cb4492c
Terenceonye <onyeweketernce@gmail.com>
2022-07-12 11:09:02 -0400

    Starting pointers ..

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _puts - prints any string to stdout
+* @str - ponter variable of type char
+* Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	{
+
+	_putchar('\n');
+}
+
+
